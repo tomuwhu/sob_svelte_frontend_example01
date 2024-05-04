c5a2703771df6f5d3553679d5e1139ba7f066f6f
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-04 14:09:32 +0200

    .

---
+++ docs/_app/immutable/entry/start.DZYvWzcv.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CFZWguqa.js";export{t as start};
--- src/routes/variables/+page.svelte
+++ src/routes/variables/+page.svelte
@@ -1,11 +1,12 @@
 <script>
+    import {base} from "$app/paths";
     var x = 10;
     $: x2 = x*x
 </script>
 <h1>SOB SvelteKit Példaprogramok</h1>
 <h3>1. oldal: Változók, Input, Output</h3>
 <div>
-<a href="https://tomuwhu.github.io/sob_svelte_frontend_example01/">Vissza a menühöz</a>
+<a href="{base}/..">Vissza a menühöz</a>
     <hr>
     <button on:click={()=>x--}>- </button>
     <input type="number" bind:value={x}>
