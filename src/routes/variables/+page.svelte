1ea75ade3137d5f9d015545ad804aa9e04286326
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-04 14:05:46 +0200

    .

--- src/routes/variables/+page.svelte
+++ src/routes/variables/+page.svelte
@@ -5,7 +5,7 @@
 <h1>SOB SvelteKit Példaprogramok</h1>
 <h3>1. oldal: Változók, Input, Output</h3>
 <div>
-<a href="/">Vissza a menühöz</a>
+<a href="https://tomuwhu.github.io/sob_svelte_frontend_example01/">Vissza a menühöz</a>
     <hr>
     <button on:click={()=>x--}>- </button>
     <input type="number" bind:value={x}>
