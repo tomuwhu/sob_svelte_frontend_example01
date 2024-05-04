b7c459851b4b6dab55037543fd6c9b5dc66bf151
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-04 13:56:09 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
 		adapter: adapter({
+
 			pages: 'docs',
 			assets: 'docs',
 			fallback: '404.html',
