c87de991c0b0230431f89b0a2486a3a39d228748
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-05 09:40:46 +0200

    .

--- src/routes/variables/+page.svelte
+++ src/routes/variables/+page.svelte
@@ -1,32 +1,4 @@
 <script>
-    var axios = require('axios');
-    var data = JSON.stringify({
-        "collection": "users",
-        "database": "sob",
-        "dataSource": "sob",
-        "projection": {
-            "_id": 1
-        }
-    });
-    var config = {
-        method: 'post',
-        url: 'https://eu-central-1.aws.data.mongodb-api.com/app/data-svqctff/endpoint/data/v1/action/findOne',
-        headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': 'je5FopKLCrSwLH3mCnKXp0IjwRwen5JNIyiMMSB9WlDjYdjK5FmAOtsd8268Vrk5',
-        'Accept': 'application/ejson'
-        },
-        data: data
-    };
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
     import {base} from "$app/paths";
     var x = 10;
     $: x2 = x*x
